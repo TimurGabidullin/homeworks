182cdb75c054709d7ca5efd93d441193d9589fe2
TimurGabidullin <Pr.Gabidullin@gmail.com>
2021-07-12 17:37:56 -0700

    hw5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {NavLink} from "react-router-dom";
+import s from './Header.module.css'
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
 
+    const [active, setActive] = useState(false)
+    let onEnterMouse = () => {
+        setActive(true)
+    }
+
+    let onLeaveMouse = () => {
+        setActive(false)
+    }
+
+    return (
+
+        <div className={active ? `${s.nav} ${s.active}` : s.nav} onMouseLeave={onLeaveMouse}>
+            <div className={s.btn} onMouseEnter={onEnterMouse}></div>
+            <ul className={s.menu}>
+                <li><NavLink to='/pre-junior' activeClassName={s.linkActive}>pre-junior</NavLink></li>
+                <li><NavLink to='/junior' activeClassName={s.linkActive}>junior</NavLink></li>
+                <li><NavLink to='/junior+' activeClassName={s.linkActive}>junior+</NavLink></li>
+            </ul>
         </div>
+
     )
 }
 
--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import {Switch, Redirect, Route} from "react-router-dom";
+import PreJunior from "./pages/PreJunior";
+import Error404 from "./pages/Error404";
+
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
+    JUNIOR: '/junior',
+    JUNIOR_PLUS: '/junior+'
     // add paths
 }
 
@@ -9,19 +15,23 @@ function Routes() {
     return (
         <div>
             {/*Switch выбирает первый подходящий роут*/}
-            {/*<Switch>*/}
+
+            <Switch>
 
             {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
             {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
-            {/*<Route path={'/'} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>*/}
 
-            {/*<Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>*/}
+            <Route exact path={'/'}  render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>
+
+            <Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>
+            <Route path={PATH.JUNIOR} render={() => <div>junior</div>}/>
+            <Route path={PATH.JUNIOR_PLUS} render={() => <div>Junior+</div>}/>
                 // add routes
 
             {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-            {/*<Route render={() => <Error404/>}/>*/}
+            <Route render={() => <Error404/>}/>
 
-            {/*</Switch>*/}
+            </Switch>
         </div>
     )
 }
