91293c759447dfd1b2e0f8532e467cf505556cc4
TimurGabidullin <Pr.Gabidullin@gmail.com>
2021-07-24 09:07:12 -0700

    hw6

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -6,6 +6,7 @@ import HW3 from "../../../p2-homeworks/h3/HW3";
 import HW4 from "../../../p2-homeworks/h4/HW4";
 import HW5 from "../../../p2-homeworks/h5/HW5";
 
+
 function App() {
     return (
         <div className={s.App}>
@@ -15,6 +16,7 @@ function App() {
             {/*<HW3/>*/}
             {/*<HW4/>*/}
             <HW5/>
+
 
         </div>
     )
