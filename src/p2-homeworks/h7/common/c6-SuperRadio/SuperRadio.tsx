0881f65bc6ad5c9c125e5580c9d71733f6dd40ab
TimurGabidullin <Pr.Gabidullin@gmail.com>
2021-08-04 18:23:06 -0700

    hw7 refactored with types

--- src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
+++ src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
@@ -3,8 +3,8 @@ import React, {ChangeEvent, InputHTMLAttributes, DetailedHTMLProps} from 'react'
 type DefaultRadioPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 type SuperRadioPropsType = DefaultRadioPropsType & {
-    options?: any[]
-    onChangeOption?: (option: any) => void
+    options?: string[]
+    onChangeOption?: (option: string) => void
 }
 
 const SuperRadio: React.FC<SuperRadioPropsType> = (
@@ -17,17 +17,12 @@ const SuperRadio: React.FC<SuperRadioPropsType> = (
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // onChange, onChangeOption
-
-
         onChangeOption && onChangeOption(e.currentTarget.value)
         onChange && onChange(e)
-
-        console.log(e.currentTarget.value)
-
     }
 
 
-    const mappedOptions: any[] = options ? options.map((o, i) => (
+    const mappedOptions: JSX.Element[]= options ? options.map((o, i) => (
         <label key={name + '-' + i}>
             <input
                 type={'radio'}
