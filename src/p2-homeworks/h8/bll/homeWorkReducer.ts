c1f4e89e94574f39ece2b43ebc8f3f6435e02e9c
TimurGabidullin <Pr.Gabidullin@gmail.com>
2021-09-17 11:13:47 -0700

    hw8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,20 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+export const homeWorkReducer = (state: UserType[], action: { type: string, payload: string | number }): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
             // need to fix
+            if (action.payload === 'up') {
+                return [...state.sort((a,b)=>b.age-a.age)]
+            }else if (action.payload === 'down') {
+                return [...state.sort((a,b)=>a.age-b.age)]
+            }
             return state
         }
         case 'check': {
-            // need to fix
-            return state
+            return [...state.filter((u) => u.age >= action.payload)]
         }
-        default: return state
+        default:
+            return state
     }
 }
