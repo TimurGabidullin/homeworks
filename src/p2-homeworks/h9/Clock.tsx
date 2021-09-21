741f3714faa300128702aac305c923e4264febd1
TimurGabidullin <Pr.Gabidullin@gmail.com>
2021-09-20 19:18:24 -0700

    hw9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -3,29 +3,33 @@ import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
+        clearInterval(timerId)
         // stop
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())// setDate
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
+        setShow(true)
         // show
     }
     const onMouseLeave = () => {
+        setShow(false)
+
         // close
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;// fix with date
+    const stringDate = `${date.getDate()}:${date.getMonth()}:${date.getFullYear()}` // fix with date
 
     return (
         <div>
