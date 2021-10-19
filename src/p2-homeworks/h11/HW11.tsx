cf7367c2a6e03d9722fdf7b8febe9912252fe6e9
TimurGabidullin <Pr.Gabidullin@gmail.com>
2021-10-18 18:29:01 -0700

    hw11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -6,6 +6,11 @@ function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
 
+
+
+
+
+
     return (
         <div>
             <hr/>
@@ -15,14 +20,25 @@ function HW11() {
             <div>
                 <span>{value1}</span>
                 <SuperRange
+                    value={value1}
+                    onChangeRange={(value) => {
+                        setValue1(value)
+                    }}
                     // сделать так чтоб value1 изменялось
                 />
             </div>
 
             <div>
                 <span>{value1}</span>
+
                 <SuperDoubleRange
                     // сделать так чтоб value1 и value2 изменялось
+                    value={[value1, value2]}
+                    onChangeRange={([value1, value2])=>{
+                        setValue1(value1)
+                        setValue2(value2)
+                    }}
+
                 />
                 <span>{value2}</span>
             </div>
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,22 +1,58 @@
-import React from 'react'
+import React, {ChangeEvent} from 'react'
+import './SuperDoubleRange.css'
+
 
 type SuperDoubleRangePropsType = {
     onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
+    value: [number, number]
+
+
+
     // min, max, step, disable, ...
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
         onChangeRange, value,
+
+
         // min, max, step, disable, ...
     }
 ) => {
     // сделать самому, можно подключать библиотеки
+    //
+
+    const onChangeCallbackMin = (e: ChangeEvent<HTMLInputElement>) => {
+        const val = Math.min(Number(e.target.value), value[1] - 1);
+        onChangeRange && onChangeRange([val, value[1]])
+    }
+
+    const onChangeCallbackMax = (e: ChangeEvent<HTMLInputElement>) => {
+        const val = Math.max(Number(e.target.value), value[0] + 1);
+        onChangeRange && onChangeRange([value[0], val])
+    }
+
 
     return (
         <>
-            DoubleRange
+            {/*DoubleRange*/}
+            <input
+                type="range"
+                value={value[0]}
+                onChange={onChangeCallbackMin}
+                className="thumb thumb--left"
+                style={{zIndex: value[0] > value[1] - 10 ? 5 : 3}}
+            />
+            <input
+                type="range"
+                value={value[1]}
+                onChange={onChangeCallbackMax}
+                className="thumb thumb--right"
+            />
+            <div className="slider">
+                <div className="slider__track" />
+                <div className="slider__range" />
+            </div>
         </>
     )
 }
